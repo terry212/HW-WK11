30711aa50f785d4dceb6d5454beca150eecf9c72
Terry C <terrycetoute@gmail.com>
2020-05-22 16:28:24 -0400

    second html route added

--- server.js
+++ server.js
@@ -2,24 +2,25 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
-
+var fs = require("fs");
 // EXPRESS CONFIGURATION
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// require("./public/assets/js/index")(app);
-
-// Routes
+// HTML Routes
 // =============================================================
-app.get("/notes", function(req, res) {
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
+});
+// API Routes
+// =============================================================
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
 });
