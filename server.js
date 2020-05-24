3d15f5e67f69eb5825190baed159c5b75026b95f
Terry C <terrycetoute@gmail.com>
2020-05-24 15:14:19 -0400

    notes api is reading the JSON and returns notes in JSON format

--- server.js
+++ server.js
@@ -10,16 +10,22 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// API Routes
+// =============================================================
+app.get("/api/notes", function (req, res) {
+    res.json(JSON.parse(fs.readFileSync(__dirname + "/db/db.json", function (err, data) {
+        if (err) throw err;
+        return data;
+    })));
+});
 // HTML Routes
 // =============================================================
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-// API Routes
-// =============================================================
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
